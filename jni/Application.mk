5c142f7200aee7ac38be3b1da6d7b8428c3a0ea4
qiudong <dong.qiu@madv360.com>
2017-05-19 18:52:28 +0800

    Archs;

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
-APP_ABI := armeabi x86 armeabi-v7a x86_64 mips
-APP_PLATFORM := android-9
+APP_ABI := armeabi x86 armeabi-v7a mips arm64-v8a x86_64 mips64
+APP_PLATFORM := android-10
 APP_OPTIM := debug
 APP_CFLAGS := -DDEBUG 
 APP_STL := gnustl_static
