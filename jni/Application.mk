427b55a1bdb42533657ab5e3fbf7af583bfb4693
chenqiu1024 <godspeed1024@gmail.com>
2017-05-20 12:08:17 +0800

    Refs;

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,5 @@
-APP_ABI := armeabi x86 armeabi-v7a mips arm64-v8a x86_64 mips64
+#APP_ABI := armeabi x86 armeabi-v7a mips arm64-v8a x86_64 mips64
+APP_ABI := all
 APP_PLATFORM := android-10
 APP_OPTIM := debug
 APP_CFLAGS := -DDEBUG 
