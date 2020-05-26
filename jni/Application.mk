7d729796e02da9360018d63d7f853dcd4121cce7
Andrey Vasiliev <13902167+roninest@users.noreply.github.com>
2020-05-26 12:31:15 +0300

    Make it compatible with ndk21

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 #APP_ABI := armeabi x86 armeabi-v7a mips arm64-v8a x86_64 mips64
 APP_ABI := all
-APP_PLATFORM := android-10
-APP_OPTIM := debug
-APP_CFLAGS := -DDEBUG 
-APP_STL := gnustl_static
+APP_PLATFORM := android-16
+APP_OPTIM := release
+APP_CFLAGS := -DNDEBUG 
+APP_STL := c++_static
